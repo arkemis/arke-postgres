1c8d76d82427f7f13b6d5030ee07cf8fdcdf4e58
Ilyich Vismara <ilyich@arkemis.it>
2026-08-04 17:54:50 +0200

    v0.7.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ArkePostgres.MixProject do
   use Mix.Project
 
-  @version "0.6.0"
+  @version "0.7.0"
   @scm_url "https://github.com/arkemis/arke-postgres"
   @site_url "https://arkehub.com"
 
